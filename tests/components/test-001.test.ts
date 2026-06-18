f3b4a1fc5e9cbfd9b9c62ace5dfe7fdd0a026f02
yashukaSan <ysahu1590@gmail.com>
2026-06-18 23:04:17 +0530

    form related test cases done

--- tests/components/test-001.test.ts
+++ tests/components/test-001.test.ts
@@ -8,4 +8,82 @@ test.describe('main page', ()=> {
 
         await expect(page).toHaveTitle(/Quiz/i);
     })
+
+    test('when all field value provided and click on Submit button', async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        const nameInput = page.locator('#name-input');
+        const ageInput = page.locator('#age-input');
+        const submitBtn = page.locator('#submit-button');
+        
+
+        await nameInput.fill("Yashuka");
+        await ageInput.fill(String(25));
+
+        await submitBtn.click()
+
+        const subjectScreen = page.locator('#subject-select-screen');
+
+        expect(subjectScreen).toBeVisible();
+    });
+
+    
+    test('when name field is provided and age field is not', async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        const nameInput = page.locator('#name-input');
+        const ageInput = page.locator('#age-input');
+        const submitBtn = page.locator('#submit-button');
+        
+
+        await nameInput.fill("Yashuka");
+        await ageInput.fill("");
+
+        await submitBtn.click()
+
+        const subjectScreen = page.locator('#subject-select-screen');
+        const ageValue = page.locator('#age-container');
+        const nameValue = page.locator('#name-conatiner');
+
+        expect(subjectScreen).toBeVisible();
+        expect(ageValue).not.toBeVisible();
+        expect(nameValue).not.toBeVisible();
+    });
+    
+    test('when age field is provided and nmae is not', async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        const nameInput = page.locator('#name-input');
+        const ageInput = page.locator('#age-input');
+        const submitBtn = page.locator('#submit-button');
+        
+
+        await nameInput.fill("");
+        await ageInput.fill(String(25));
+
+        await submitBtn.click()
+
+        const subjectScreen = page.locator('#subject-select-screen');
+
+        expect(subjectScreen).not.toBeVisible();
+    });
+
+    
+    test('when no field value provided and click on Submit button', async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        const nameInput = page.locator('#name-input');
+        const ageInput = page.locator('#age-input');
+        const submitBtn = page.locator('#submit-button');
+        
+
+        await nameInput.fill("");
+        await ageInput.fill("");
+
+        await submitBtn.click()
+
+        const subjectScreen = page.locator('#subject-select-screen');
+
+        expect(subjectScreen).not.toBeVisible();
+    })
 })
